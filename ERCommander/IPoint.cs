8bdfd3df4f957d7ba4664339c9d9180a4c2557e2
lameR <erpresents@gmail.com>
2021-04-16 19:13:49 +0300

    Create IPoint.cs

---
+++ ERCommander/IPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERCommander
+{
+    interface IPoint
+    {
+        int X { get; set; }
+        int Y { get; set; }
+
+        string Sym { get; set; }
+
+        void Draw();
+    }
+}
