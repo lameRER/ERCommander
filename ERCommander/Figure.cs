418f7561f860fdbaf381c447a00d608cb9005b75
lameR <erpresents@gmail.com>
2021-04-16 19:17:50 +0300

    Create Figure.cs

---
+++ ERCommander/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERCommander
+{
+    class Figure 
+    {
+        protected List<Point> pList;
+
+      
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+
+    }
+}
