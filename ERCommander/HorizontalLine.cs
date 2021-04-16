e95962440b3f258339ae03a591fdc4982a9efb72
lameR <erpresents@gmail.com>
2021-04-16 19:15:56 +0300

    Create HorizontalLine.cs

---
+++ ERCommander/HorizontalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERCommander
+{
+    class HorizontalLine : Figure
+    {
+        /// <summary>
+        /// Отрисовка горизонтальной линии
+        /// </summary>
+        /// <param name="xLeft">Начало оси X</param>
+        /// <param name="XRight">Конеч оси X</param>
+        /// <param name="y">Начало оси Y</param>
+        /// <param name="sym">Символ</param>
+        public HorizontalLine(int xLeft, int XRight, int y, string sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x < XRight; x++)
+            {
+                var p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
