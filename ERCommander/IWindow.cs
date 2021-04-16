0e1ee51b489b3a57bd3c5e565d837c90775ab0b9
lameR <erpresents@gmail.com>
2021-04-16 19:16:32 +0300

    Create IWindow.cs

---
+++ ERCommander/IWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERCommander
+{
+    interface IWindow
+    {
+        abstract  int XLeft { get; }
+        abstract int XRight { get; }
+        abstract int YUp { get; }
+        abstract int YDown { get; }
+
+        abstract bool Tabs { get; set; }
+    }
+}
