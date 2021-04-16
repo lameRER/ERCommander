92fe877e27781cd7a50e8ca04dcfc25ece49f889
lameR <erpresents@gmail.com>
2021-04-16 19:21:50 +0300

    Create IBlock.cs

---
+++ ERCommander/IBlock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERCommander
+{
+    interface IBlock
+    {
+        /// <summary>
+        /// Координаты блока по оси X
+        /// </summary>
+        int[] X { get; set; }
+        /// <summary>
+        /// Координаты блока по оси Y
+        /// </summary>
+        int[] Y { get; set; }
+    }
+}
