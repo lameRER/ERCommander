34f05ae4dc0e723037f2fd19efe2009dea7ba51d
lameR <erpresents@gmail.com>
2021-04-16 19:20:39 +0300

    Create IBaseItem.cs

---
+++ ERCommander/IBaseItem.cs
@@ -0,0 +1,18 @@
+﻿namespace ERCommander
+{
+    public interface IBaseItem
+    {
+        /// <summary>
+        /// Полный путь к элементу
+        /// </summary>
+        string MainPath { get; set; }
+        /// <summary>
+        /// Имя элемента
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// Размер
+        /// </summary>
+        long? Size { get; set; }
+    }
+}
