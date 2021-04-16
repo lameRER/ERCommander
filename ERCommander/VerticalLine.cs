7a03231526f3bd08aed15d0c3c38796e7af5be15
lameR <erpresents@gmail.com>
2021-04-16 19:16:14 +0300

    Create VerticalLine.cs

---
+++ ERCommander/VerticalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERCommander
+{
+    class VerticalLine : Figure
+    {
+        /// <summary>
+        /// Отрисовка горизонтальной линии
+        /// </summary>
+        /// <param name="xLeft">Начало оси Y</param>
+        /// <param name="XRight">Конеч оси Y</param>
+        /// <param name="y">Начало оси X</param>
+        /// <param name="sym">Символ</param>
+        public VerticalLine(int yLeft, int yRight, int x, string sym)
+        {
+            pList = new List<Point>();
+            for (int y = yLeft; y < yRight; y++)
+            {
+                var p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+}
